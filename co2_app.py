c63438dfd3d0e3cfcfcaec23c8b3caa01dbd7a00
Loïc Lannelongue <l.lannelongue@gmail.com>
2020-01-16 21:46:41 +0000

    Adjust location/cores based on the providers Added some "others" options Adjusted the display of the PUE input for when it's relevant Other display details

--- co2_app.py
+++ co2_app.py
@@ -35,13 +35,17 @@ data_dir = os.path.join(os.path.abspath(''),'data')
 cpu_df = pd.read_csv(os.path.join(data_dir, "TDP_cpu.csv"),
                      sep=',', skiprows=1)
 cpu_df.drop(['source'], axis=1, inplace=True)
-cpu_dict = pd.Series(cpu_df.TDP.values,index=cpu_df.model).to_dict()
 
 ### GPU ###
 gpu_df = pd.read_csv(os.path.join(data_dir, "TDP_gpu.csv"),
                      sep=',', skiprows=1)
 gpu_df.drop(['source'], axis=1, inplace=True)
-gpu_dict = pd.Series(gpu_df.TDP.values,index=gpu_df.model).to_dict()
+
+# Dict of dict with all the possible models
+# e.g. {'CPU': {'Intel(R) Xeon(R) Gold 6142': 150, 'Core i7-10700K': 125, ...
+cores_dict = dict()
+cores_dict['CPU'] = pd.Series(cpu_df.TDP.values,index=cpu_df.model).to_dict()
+cores_dict['GPU'] = pd.Series(gpu_df.TDP.values,index=gpu_df.model).to_dict()
 
 ### PUE ###
 pue_df = pd.read_csv(os.path.join(data_dir, "servers_PUE.csv"),
@@ -49,7 +53,7 @@ pue_df = pd.read_csv(os.path.join(data_dir, "servers_PUE.csv"),
 pue_df.drop(['source'], axis=1, inplace=True)
 
 ### HARDWARE ###
-hardware_df = pd.read_csv(os.path.join(data_dir, "servers_hardware.csv"),
+hardware_df = pd.read_csv(os.path.join(data_dir, "providers_hardware.csv"),
                           sep=',', skiprows=1)
 hardware_df.drop(['source'], axis=1, inplace=True)
 
@@ -66,7 +70,20 @@ impact_dict = pd.Series(impact_df.impact.values,index=impact_df.location).to_dic
 
 ### CLOUD DATACENTERS ###
 cloudDatacenters_df = pd.read_csv(os.path.join(data_dir, "cloudProviders_datacenters.csv"),
-                                  sep=',')
+                                  sep=',', skiprows=1)
+
+### LOCAL DATACENTERS ###
+localDatacenters_df = pd.read_csv(os.path.join(data_dir, "localProviders_datacenters.csv"),
+                                  sep=',', skiprows=1)
+
+datacenters_df = pd.concat([cloudDatacenters_df, localDatacenters_df], axis = 1)
+datacenters_dict = dict()
+for col in datacenters_df.columns:
+    datacenters_dict[col] = list(datacenters_df[col].dropna().values)
+
+### PROVIDERS CODES AND NAMES ###
+providersNames_df = pd.read_csv(os.path.join(data_dir, "providersNamesCodes.csv"),
+                                  sep=',', skiprows=1)
 
 
 ############
@@ -78,23 +95,11 @@ colors = {
     'text': '#4c3327'
 }
 
-# Dict of dict of dict ... with all the possible locations.
-# e.g. {'Europe': {'Belgium': {'St. Ghislain': 'BEL-StGhislain'} ...
-allLocations = dict()
-for continent in set(impact_df.continentName):
-    allLocations[continent] = dict()
-
-    list_countries = list(set(impact_df.loc[impact_df.continentName == continent, 'countryName']))
-    for country in list_countries:
-        subDf = impact_df.loc[(impact_df.continentName == continent)&(impact_df.countryName == country)]
-        allLocations[continent][country] = pd.Series(subDf.location.values,index=subDf.cityName).to_dict()
-        # allLocations[continent][country] = list(set(impact_df.loc[(impact_df.continentName == continent)&(impact_df.countryName == country), 'cityName']))
-
-# Dict of dict with all the possible models
-# e.g. {'CPU': {'Intel(R) Xeon(R) Gold 6142': 150, 'Core i7-10700K': 125, ...
-allCores = dict()
-allCores['CPU'] = pd.Series(cpu_df.TDP.values,index=cpu_df.model).to_dict()
-allCores['GPU'] = pd.Series(gpu_df.TDP.values,index=gpu_df.model).to_dict()
+allPlatforms_names = dict({
+    'Cloud computing':'cloudComputing',
+    'Local server':'localServer',
+    'Local desktop':'localDesktop'
+})
 
 app.layout = html.Div(
     style = {'backgroundColor':colors['background'],'padding': 100},
@@ -109,16 +114,24 @@ app.layout = html.Div(
                     'padding': 10
                 }),
 
-        ### RUN TIME ###
+        ### SELECT COMPUTING PLATFORM ###
 
         html.Div(
             style = {'columnCount': 1,'padding': 10},
-            children = [
-                html.Label('Runtime (hours)'),
-                dcc.Input(type='number',
-                          id="runTime_input",
-                          value=5),
-            ]),
+            children=[
+                html.Label('On what platform are you running your tool:'),
+
+                dcc.Dropdown(
+                    options=[{'label': k, 'value': v} for k,v in list(providersNames_df.loc[:,['platformName',
+                                                                                               'platformType']].drop_duplicates().apply(tuple, axis=1)) +
+                             [('Personal computer', 'personalComputer')]],
+                    id="platformType_dropdown", value='cloudComputing'),
+
+                html.Div(children = dcc.Dropdown(id="provider_dropdown", value='gcp'),
+                         style = {'display': 'none'},
+                         id = 'provider_div')
+            ]
+        ),
 
         ### COMPUTING CORES ###
 
@@ -126,9 +139,9 @@ app.layout = html.Div(
             style = {'columnCount': 1,'padding': 10},
             children = [
                 html.Label('What type of computing core are you using?'),
-                dcc.Dropdown(
-                    options=[{'label': k, 'value': k} for k in allCores.keys()],
-                    id="coreType_dropdown", value='CPU'),
+
+                dcc.Dropdown(id="coreType_dropdown", value = 'CPU'),
+
                 dcc.Dropdown(id = "coreModel_dropdown")]),
 
         ### NUMBER OF CORES ###
@@ -141,15 +154,24 @@ app.layout = html.Div(
                           id="numberCores_input",
                           value=1)]),
 
+        ### RUN TIME ###
+
+        html.Div(
+            style={'columnCount': 1, 'padding': 10},
+            children=[
+                html.Label('Runtime (hours)'),
+                dcc.Input(type='number',
+                          id="runTime_input",
+                          value=5),
+            ]),
+
         ### LOCATION ###
 
         html.Div(
             style = {'columnCount': 1,'padding': 10},
             children = [
                 html.Label('Select Location'),
-                dcc.Dropdown(
-                    options=[{'label': k, 'value': k} for k in allLocations.keys()],
-                    id="location_continent_dropdown",value='North America'),
+                dcc.Dropdown(id="location_continent_dropdown",value='North America'),
 
                 dcc.Dropdown(id="location_country_dropdown", value="USA"),
 
@@ -158,17 +180,38 @@ app.layout = html.Div(
         ### PUE ###
 
         html.Div(
-            style = {'columnCount': 1,'padding': 10},
+            style = {'columnCount': 1,'padding': 10,'display': 'none'},
             children = [
-                html.Label('Power Usage Efficiency'),
+                html.Label('Do you know the Power Usage Efficiency (PUE) of your local datacenter?'),
+                dcc.RadioItems(
+                    options=[
+                        {'label': 'Yes', 'value': 'Yes'},
+                        {'label': 'No', 'value': 'No'}
+                    ],
+                    id = 'pue_radio',
+                    value='No')
+            ],
+            id = 'PUEquestion_div'
+        ),
+
+        html.Div(
+            style = {'columnCount': 1,'padding': 10,'display': 'none'},
+            children = [
                 dcc.Input(min=1,
                           type='number',
                           id="PUE_input",
-                          value=1.58)]),
+                          value=pue_df.loc[pue_df.provider == 'Unknown','PUE'][0])
+            ],
+            id = 'PUEinput_div'
+        ),
 
         ### BUTTON ###
 
-        html.Button('Compute', id='button'),
+        html.Div(
+            style = {'columnCount': 1,'padding': 10},
+            children = [html.Button('Compute', id='button')]
+        ),
+
 
         ### OUTPUT ###
 
@@ -181,27 +224,135 @@ app.layout = html.Div(
 # APP OUTPUT #
 ##############
 
-# This callback adjusts the list of computing cores to choose from (CPU or GPU)
+### PLATFORM AND PROVIDER ###
+
+# This callback shows or hides the choice of provider
+@app.callback(
+    Output('provider_div', 'style'),
+    [Input('platformType_dropdown', 'value')])
+def display_provider(selected_platform):
+    # if selected_platform == 'personalComputer':
+    if selected_platform in ['cloudComputing','localServer']:
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+@app.callback(
+    Output('provider_dropdown', 'options'),
+    [Input('platformType_dropdown', 'value')])
+def set_providers_options(selected_platform):
+    availableOptions = providersNames_df.loc[providersNames_df.platformType == selected_platform]
+    return [{'label': k, 'value': v} for k,v in list(zip(availableOptions.providerName, availableOptions.provider))+[("Other","other")]]
+
+### COMPUTING CORES ###
+
+@app.callback(
+    Output('coreType_dropdown', 'options'),
+    [Input('provider_dropdown', 'value'),
+     Input('platformType_dropdown', 'value')])
+def set_coreType_options(selected_provider, selected_platform):
+    if (selected_provider == 'other')|(selected_platform in ['personalComputer','cloudComputing']):
+        availableOptions = cores_dict.keys()
+    else:
+        availableOptions = list(set(hardware_df.loc[hardware_df.provider == selected_provider, 'type']))
+    return [{'label': k, 'value': k} for k in sorted(availableOptions)]
+
+# This callback adjusts the list of computing cores to choose from (models)
 @app.callback(
     Output('coreModel_dropdown', 'options'),
-    [Input('coreType_dropdown', 'value')])
-def set_coreModels_options(selected_coreType):
-    return [{'label': k, 'value': k} for k in allCores[selected_coreType].keys()]
+    [Input('coreType_dropdown', 'value'),
+     Input('provider_dropdown','value'),
+     Input('platformType_dropdown', 'value')])
+def set_coreModels_options(selected_coreType,selected_provider,selected_platform):
+    if (selected_provider == 'other')|(selected_platform in ['personalComputer','cloudComputing']):
+        availableOptions = sorted(list(cores_dict[selected_coreType].keys()))
+    else:
+        availableOptions = sorted(hardware_df.loc[(hardware_df.type == selected_coreType)&(
+                hardware_df.provider == selected_provider), 'model'].tolist())
+    return [{'label': k, 'value': k} for k in availableOptions]
 
-# This callback adjusts the list of countries to choose from depending on the continent
+### LOCATION ###
+
+# This callback adjusts the list of continents to choose from depending on the provider
+@app.callback(
+    Output('location_continent_dropdown','options'),
+    [Input('provider_dropdown', 'value'),
+     Input('platformType_dropdown', 'value')])
+def set_continents_options(selected_provider,selected_platform):
+    if (selected_provider == 'other')|(selected_platform == 'personalComputer'):
+        availableOptions = list(set(impact_df.loc[:,
+                                                  'continentName']))
+    else:
+        availableOptions = list(set(impact_df.loc[impact_df.location.isin(datacenters_dict[selected_provider]),
+                                              'continentName']))
+    return [{'label': k, 'value': k} for k in sorted(availableOptions)]
+
+# This callback adjusts the list of countries to choose from depending on the continent & the provider
 @app.callback(
     Output('location_country_dropdown', 'options'),
-    [Input('location_continent_dropdown', 'value')])
-def set_countries_options(selected_continent):
-    return [{'label': i, 'value': i} for i in allLocations[selected_continent].keys()]
+    [Input('location_continent_dropdown', 'value'),
+     Input('provider_dropdown', 'value'),
+     Input('platformType_dropdown', 'value')])
+def set_countries_options(selected_continent, selected_provider,selected_platform):
+    if (selected_provider == 'other')|(selected_platform == 'personalComputer'):
+        availableOptions = list(set(impact_df.loc[(impact_df.continentName == selected_continent), 'countryName']))
+    else:
+        availableOptions = list(set(impact_df.loc[(impact_df.location.isin(datacenters_dict[selected_provider])) & (
+            impact_df.continentName == selected_continent), 'countryName']))
+    return [{'label': k, 'value': k} for k in sorted(availableOptions)]
 
-# and this ones adjusts the list of cities depending on the country
+# and this one adjusts the list of cities depending on the country & the provider
 @app.callback(
     Output('location_city_dropdown', 'options'),
     [Input('location_continent_dropdown', 'value'),
-    Input('location_country_dropdown', 'value')])
-def set_cities_options(selected_continent, selected_country):
-    return [{'label': k, 'value': v} for k,v in allLocations[selected_continent][selected_country].items()]
+     Input('location_country_dropdown', 'value'),
+     Input('provider_dropdown', 'value'),
+     Input('platformType_dropdown', 'value')])
+def set_cities_options(selected_continent, selected_country,selected_provider,selected_platform):
+    if (selected_provider == 'other')|(selected_platform == 'personalComputer'):
+        availableOptions = impact_df.loc[(impact_df.continentName == selected_continent) & (
+                impact_df.countryName == selected_country)]
+    else:
+        availableOptions = impact_df.loc[(impact_df.location.isin(datacenters_dict[selected_provider])) & (
+                impact_df.continentName == selected_continent) & (
+                impact_df.countryName == selected_country)]
+    availableOptions = availableOptions.sort_values(by=['cityName'])
+    return [{'label': k, 'value': v} for k,v in zip(availableOptions.cityName, availableOptions.location)]
+
+### PUE ###
+
+# This callback shows or hides the PUE question depending on the different answers
+@app.callback(
+    Output('PUEquestion_div','style'),
+    [Input('location_city_dropdown','value'),
+     Input('platformType_dropdown', 'value'),
+     Input('provider_dropdown', 'value')]
+)
+def display_pue_question(selected_datacenter, selected_platform, selected_provider):
+    providers_knownPUE = list(set(pue_df.provider))
+
+    if selected_platform in ['cloudComputing','personalComputer']:
+        return {'columnCount': 1,'padding': 10,'display': 'none'}
+
+    elif selected_provider in providers_knownPUE:
+        return {'columnCount': 1,'padding': 10,'display': 'none'}
+
+    else:
+        return {'columnCount': 1,'padding': 10,'display': 'block'}
+
+# And then asks for PUE input if necessary
+@app.callback(
+    Output('PUEinput_div','style'),
+    [Input('pue_radio', 'value')]
+)
+def display_pue_input(answer_pue):
+    if answer_pue == 'No':
+        return {'columnCount': 1,'padding': 10,'display': 'none'}
+    else:
+        return {'columnCount': 1,'padding': 10,'display': 'block'}
+
+
+### OUTPUT ###
 
 # This callback updates the output
 @app.callback(
@@ -212,33 +363,34 @@ def set_cities_options(selected_continent, selected_country):
      State(component_id="numberCores_input", component_property="value"),
      State(component_id="runTime_input", component_property="value"),
      State(component_id="location_city_dropdown", component_property="value"),
-     State(component_id="PUE_input", component_property="value")
+     State(component_id="PUE_input", component_property="value"),
+     State('platformType_dropdown', 'value')
      ])
-def update_output(n_clicks, coreType, coreModel, n_cores, runTime, location, PUE):
+def update_output(n_clicks, coreType, coreModel, n_cores, runTime, location, PUE, selected_platform):
     if n_clicks is None:
         # We only display the output when the button is clicked
         raise PreventUpdate
 
     else:
-        corePower = allCores[coreType][coreModel]
+        corePower = cores_dict[coreType][coreModel]
         impactValue = impact_df.loc[impact_df.location == location, "impact"].values[0]
 
-        output = "{}: {} \n".format(coreModel, corePower)
-        output += "n_cores: {} \n".format(n_cores)
-        output += "runTime: {} \n".format(runTime)
-        output += "{}: {} \n".format(location, impactValue)
-        output += "PUE: {} \n".format(PUE)
-        #dividing by 1000 converts to kW.. so this is in g
-        energy_consumption = runTime * PUE * n_cores * corePower * impactValue / 1000
-        #convert to kg then to pounds
+        if selected_platform == 'personalComputer':
+            PUE_used = 1
+        else:
+            PUE_used = PUE
+
+        # dividing by 1000 converts to kW.. so this is in g
+        energy_consumption = runTime * PUE_used * n_cores * corePower * impactValue / 1000
+        # convert to kg then to pounds
         energy_consumption_lbs=energy_consumption*0.453592/1000
 
         ### CONTEXT ###
 
-        #convert to % of flight
+        # convert to % of flight
         co2_flight_ny_sf=1984# in lbsCO2eq (from stubell)
         car_co2=404# grams of CO2 per mile from EPA US https://www.epa.gov/greenvehicles/greenhouse-gas-emissions-typical-passenger-vehicle
-        #if job is too small to compare with flight then look at car mileage (can change this)
+        # if job is too small to compare with flight then look at car mileage (can change this)
         if energy_consumption_lbs > co2_flight_ny_sf*0.10:
             #percentage of flight
             context=round(float(energy_consumption_lbs)/co2_flight_ny_sf,2)
@@ -259,20 +411,18 @@ def update_output(n_clicks, coreType, coreModel, n_cores, runTime, location, PUE
         energy_consumption=round(energy_consumption,2)
 
         #outputting
-        output += "/// Energy Consumption: {:.2f}g CO2e".format(energy_consumption)
-
-        output2 = f'''
+        output = f'''
         Summary of the parameters:
         - {n_cores} {coreModel}: {corePower} W
         - run time: {runTime} hours
         - {location}: {impactValue} g CO2e / kWh
-        - PUE: {PUE}
+        - PUE: {PUE_used}
 
         **Your carbon footprint is {energy_consumption} g CO2e**
 
         **{context_str}**
         '''
-        return output2
+        return output
 
 if __name__ == '__main__':
     # allows app to update when code is changed!
