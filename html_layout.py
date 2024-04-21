0a7ea92e5b9e6956f2c72c7329d735da108a4725
Loïc Lannelongue <loic.lannelongue@posteo.net>
2024-04-21 18:47:55 +0100

    Removed map and moved formula up

--- html_layout.py
+++ html_layout.py
@@ -818,26 +818,37 @@ def create_appLayout(
                 className='container footer preprint'
             ),
 
-            #### MAP ####
+            #### FORMULA ####
 
             html.Div(
                 [
-                    html.H2("Carbon Intensity across the world"),
-                    html.Div(
-                        [
-                            loading_wrapper(
-                                dcc.Graph(
-                                    figure=mapCI,
-                                    className='graph',
-                                    config={'displaylogo': False},
-                                ),
-                            ),
-                        ],
-                        className='graph-container'
-                    )
+                    html.H2("The formula"),
 
+                    dcc.Markdown('''
+                        The carbon footprint is calculated by estimating the energy draw of the algorithm
+                        and the carbon intensity of producing this energy at a given location:
+
+                        `carbon footprint = energy needed * carbon intensity`
+
+                        Where the energy needed is: 
+
+                        `runtime * (cores power draw cores * usage + memory power draw) * PUE * PSF`
+
+                        The power draw of the computing cores depends on the model and number of cores, 
+                        while the memory power draw only depends on the size of memory __available__. 
+                        The usage factor corrects for the real core usage (default is 1, i.e. full usage).
+                        The PUE (Power Usage Effectiveness) measures how much extra energy is needed 
+                        to operate the data centre (cooling, lighting etc.). 
+                        The PSF (Pragmatic Scaling Factor) is used to take into account multiple identical runs 
+                        (e.g. for testing or optimisation).
+
+                        The Carbon Intensity depends on the location and the technologies used to produce electricity.
+                        If you want to check out the carbon intensity in real time, and see discrepancies between countries,
+                        check out the [ElectricityMap website](https://app.electricitymaps.com/map).
+                        Also, note that __the "energy needed" indicated at the top of this page is independent of the location.__
+                        ''')
                 ],
-                className="container maps",
+                className='container formula'
             ),
 
             #### DEFINITIONS ####
@@ -924,37 +935,6 @@ def create_appLayout(
                     ''')
                 ],
                 className='container to-do'
-            ),
-
-            #### FORMULA ####
-
-            html.Div(
-                [
-                    html.H2("The formula"),
-
-                    dcc.Markdown('''
-                    The carbon footprint is calculated by estimating the energy draw of the algorithm
-                    and the carbon intensity of producing this energy at a given location:
-
-                    `carbon footprint = energy needed * carbon intensity`
-
-                    Where the energy needed is: 
-
-                    `runtime * (power draw for cores * usage + power draw for memory) * PUE * PSF`
-
-                    The power draw for the computing cores depends on the model and number of cores, 
-                    while the memory power draw only depends on the size of memory available. 
-                    The usage factor corrects for the real core usage (default is 1, i.e. full usage).
-                    The PUE (Power Usage Effectiveness) measures how much extra energy is needed 
-                    to operate the data centre (cooling, lighting etc.). 
-                    The PSF (Pragmatic Scaling Factor) is used to take into account multiple identical runs 
-                    (e.g. for testing or optimisation).
-
-                    The Carbon Intensity depends on the location and the technologies used to produce electricity.
-                    But note that __the "energy needed" indicated at the top of this page is independent of the location.__
-                    ''')
-                ],
-                className='container formula'
             ),
 
             #### HOW TO REPORT ####
