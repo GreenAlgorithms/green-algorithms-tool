52d3500798cc4c66997c45ed5019217e55b0c058
Navirah Kamal <navirahkamal@gmail.com>
2026-05-13 11:01:54 +0100

    Update CI data configuration with files from all past versions + fixed all zones logic

--- utils/handle_inputs.py
+++ utils/handle_inputs.py
@@ -303,14 +303,21 @@ def load_data(version: str):
     data_dict.pueDefault_dict = pd.Series(pue_df.PUE.values, index=pue_df.provider).to_dict()
 
     ### CARBON INTENSITY BY LOCATION ###
-    with open(data_paths_v["CI"], 'r') as f:
+    with open(os.path.join(os.path.abspath(''), 'utils/CI_data_config.yml'), 'r') as f:
         ci_yml = yaml.load(f, Loader=yaml.SafeLoader)
+        ci_yml = ci_yml[data_paths_v["CI"]] # Picking the CI data source and config according to the data version
+
     CI_df = pd.read_csv(os.path.join(data_dir, ci_yml['file_path']), sep=',', skiprows=ci_yml['skiprows'], keep_default_na=False, na_values=[])
 
-    # Invert the mapping: file column names -> expected column names
-    col_mapping = {v: k for k, v in ci_yml['column_mapping'].items()}
-    CI_df = CI_df.rename(columns=col_mapping)
-    CI_df = CI_df[list(ci_yml['column_mapping'].keys())] # Keeping only the relevant columns
+    if ci_yml.get('column_mapping', False):
+        # Invert the mapping: file column names -> expected column names
+        col_mapping = {v: k for k, v in ci_yml['column_mapping'].items()}
+        CI_df = CI_df.rename(columns=col_mapping)
+        CI_df = CI_df[list(ci_yml['column_mapping'].keys())] # Keeping only the relevant columns
+
+    if ci_yml.get('normalise_region', False):
+        # Where zone name (region) == country name, replace with 'Any' 
+        CI_df.loc[CI_df['regionName'] == CI_df['countryName'], 'regionName'] = 'Any'
     
     #check_CIcountries_df(CI_df)
     assert len(set(CI_df.location)) == len(CI_df.location)
@@ -519,11 +526,9 @@ def availableOptions_region(selected_continent: str,selected_country: str, data:
     if availableOptions_data is not None:
         availableOptions_names = list(availableOptions_data.keys())
         availableOptions_names.sort()
-        
-        # Move Any/entire country as region to the first row if available:
-        priority = [x for x in ['Any', selected_country] if x in availableOptions_names]
-        availableOptions_names = priority + [x for x in availableOptions_names if x not in priority]
-
+        if 'Any' in availableOptions_names:
+            availableOptions_names.remove('Any')
+            availableOptions_names = ['Any'] + availableOptions_names
         availableOptions_loc = [availableOptions_data[x]['location'] for x in availableOptions_names]
     else:
         availableOptions_loc = []
